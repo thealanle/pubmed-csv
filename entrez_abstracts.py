a6d3c983327ddf2daac643aa8110e84800b592e1
alan <35761977+thealanle@users.noreply.github.com>
2018-05-11 12:56:46 -0700

    Implemented config file for user e-mail address and API key.

--- entrez_abstracts.py
+++ entrez_abstracts.py
@@ -3,16 +3,53 @@
 from Bio import Entrez
 import csv
 
+
+# The config file should consist of two lines, of the format:
+# <user@example.com>
+# <NCBI_api_key>
+CONFIG_PATH = 'pubmed-csv-config.txt'
+
 # File containing line-separated Pubmed IDs
-PMID_FILE = 'pmid_input/pmid_sample.txt'
-NCBI_EMAIL = 'user@example.com'
-NCBI_KEY = '####################################'
+PMID_PATH = 'pmid_input/pmid_sample.txt'
+
+
+def config_setup(path):
+    """
+    Given a path to the config file, returns a tuple:
+    (user@example.com, <NCBI_api_key>)
+    If the config file does not exist, user is prompted to create one.
+    """
+
+    email = None
+    api_key = None
+
+    try:
+        with open(path, 'r') as config:
+            print("Config file found at {}".format(path))
+            email = config.readline()
+            api_key = config.readline()
+    except FileNotFoundError:
+        query = input(
+            "No config file found at '{}'. Create a config file now? y/N\n >".format(path)).lower()
+        if query != 'y':
+            print("No config file will be created.")
+            pass
+        else:
+            email = input("Input an e-mail address for use with NCBI:\n>")
+            api_key = input("Input NCBI API key:\n>")
+            with open(path, 'w') as config:
+                config.write(email)
+                config.write(api_key)
+            print("Config file created at {}".format(path))
+
+    return (email, api_key)
 
 
 def ids_from_file(filename):
     """
-    Given a file with a PMID on each line, returns a list of lists containing PMIDs as strings.
-    Due to memory restrictions with Entrez.read, PMIDs are batched in groups of 200.
+    Given a file with a PMID on each line, returns a list of lists containing
+    PMIDs as strings. Due to memory restrictions with Entrez.read, PMIDs are
+    batched in groups of 200.
     """
 
     result = [[]]  # A list containing an empty list.
@@ -32,7 +69,8 @@ def ids_from_file(filename):
 
 def get_abs_text(record):
     """
-    Given a record from Entrez.read(handle), attempt to return a string containing the text of the article's abstract.
+    Given a record from Entrez.read(handle), attempt to return a string
+    containing the text of the article's abstract.
     """
 
     result = []
@@ -74,10 +112,9 @@ def write_csv_file(pmid, title, abstract, f_out):
 
 if __name__ == '__main__':
 
-    pmid_list = ids_from_file(PMID_FILE)
+    Entrez.email, Entrez.api_key = config_setup(CONFIG_PATH)
 
-    Entrez.email = NCBI_EMAIL
-    Entrez.api_key = NCBI_KEY
+    pmid_list = ids_from_file(PMID_PATH)
 
     for sublist in pmid_list:
 
